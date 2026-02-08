633027092c07f62ec1a11e71a3bbd4527a8c815e
ratulislam46 <arfanratul46@gmail.com>
2026-02-09 00:27:45 +0600

    Added development branch

--- src/app/page.js
+++ src/app/page.js
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div>
-      <h2>Hello world</h2>
+      <h2 className="text-2xl">Hello world</h2>
     </div>
   );
 }
