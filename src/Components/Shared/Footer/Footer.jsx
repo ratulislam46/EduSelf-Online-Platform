32e7114cb02ad9035e321b7616708323297228d4
ratulislam46 <arfanratul46@gmail.com>
2026-02-09 00:43:59 +0600

    Modified Footer section

--- src/Components/Shared/Footer/Footer.jsx
+++ src/Components/Shared/Footer/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Footer = () => {
     return (
         <div>
-            <h2>Footer section</h2>
+            <h2 className='border'>Footer section</h2>
         </div>
     );
 };
