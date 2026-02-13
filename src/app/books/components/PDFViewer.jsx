ed46a3c170049f723cc05b1e02a129b714292c2e
ratulislam46 <arfanratul46@gmail.com>
2026-02-13 22:31:28 +0600

    Updated PDF viewr component and fixed some error

--- src/app/books/components/PDFViewer.jsx
+++ src/app/books/components/PDFViewer.jsx
@@ -2,22 +2,22 @@
 import React from 'react';
 
 const PDFViewer = ({ book, onBack }) => {
-  return (
-    <div className="animate-in fade-in duration-500 h-[90vh] flex flex-col">
-      <div className="flex justify-between items-center mb-4 bg-white p-4 rounded-2xl shadow-sm">
-        <button onClick={onBack} className="text-red-500 font-bold">✕ Close Reader</button>
-        <h3 className="font-bold truncate max-w-[200px] md:max-w-md">{book.bookName}</h3>
-        <a href={book.pdfUrl} download className="bg-indigo-600 text-white px-4 py-2 rounded-xl text-sm font-bold">Download</a>
-      </div>
-      <div className="flex-1 bg-gray-200 rounded-2xl overflow-hidden shadow-inner">
-        <iframe 
-          src={`${book.pdfUrl}#toolbar=0`} 
-          className="w-full h-full border-none"
-          title={book.bookName}
-        />
-      </div>
-    </div>
-  );
+    return (
+        <div className="animate-in fade-in duration-500 h-[90vh] flex flex-col">
+            <div className="flex justify-between items-center mb-4  p-4">
+                <button onClick={onBack} className="text-red-500 font-bold px-4 py-2 rounded-xl text-sm border cursor-pointer">✕ Close Reader</button>
+                <h3 className="font-bold truncate max-w-[200px] md:max-w-md">{book.bookName}</h3>
+                <a href={book.pdfUrl} download className="bg-indigo-600 text-white px-4 py-2 rounded-xl text-sm font-bold">Download</a>
+            </div>
+
+            {/* Book page/PDF file show */}
+            <div className="flex-1 bg-gray-200 rounded-2xl overflow-hidden shadow-inner">
+                <div>
+                    <p className='text-gray-500 text-4xl text-center mt-50'><span className='text-amber-500'>{book.bookName}</span> Book Pages or PDF cooming soon</p>
+                </div>
+            </div>
+        </div>
+    );
 };
 
 export default PDFViewer;
